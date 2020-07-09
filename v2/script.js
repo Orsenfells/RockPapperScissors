48efd3cfefaf3a9e59158567d8d14e450a9147f8
Leo <leonelsoto17@gmail.com>
2020-07-08 20:38:34 -0500

    font change, game functions as normal

--- v2/script.js
+++ v2/script.js
@@ -1,9 +1,14 @@
-let playerWins = 0;
-let computerWins = 0;
+
 let computerMoves = ["rock", "paper", "scissors"];
-let playerChoice = document.querySelectorAll('.playerChoice');
-let playerMove = document.querySelector('.playerMove')
-let computerMove = document.querySelector('.computerMove')
+let playerChoice  = document.querySelectorAll('.playerChoice');
+let playerMove    = document.querySelector('.playerMove')
+let computerMove  = document.querySelector('.computerMove')
+let playerWins    = document.querySelector('.wins');
+let computerWins  = document.querySelector('.losses');
+let totalTies     = document.querySelector('.ties');
+let wins = 0;
+let losses = 0;
+let ties = 0;
 function computerPlay() {
     return computerMoves[Math.floor(Math.random()*computerMoves.length)];
 }
@@ -11,10 +16,48 @@ function play() {
     let computer = computerPlay();
     let player = this.innerText.toLowerCase();
     playerMove.innerText = player;
-    computerMove.innerText = computer; 
+    computerMove.innerText = computer;
+
+    switch(player) {
+        case 'rock':
+            playRock(computer);
+            break;
+        case 'paper':
+            playPaper(computer);
+            break;
+        case 'scissors':
+            playScissors(computer);
+            break;
+    }
+    score(); 
+    return
 }
-function playRound() {
-    
+function score() {
+    playerWins.innerText = `Player Wins: ${wins}`;
+    computerWins.innerText = `Computer Wins: ${losses}`;
+    totalTies.innerText = `Ties ${ties}`;
+    return
+}
+function playRock(computerMove) {
+    if(computerMove == 'scissors') {
+        return wins += 1;
+    } else if(computerMove == 'paper') {
+       return losses += 1;
+    } else return ties += 1;   
+}
+function playPaper(computerMove) {
+    if(computerMove == 'rock') {
+        return wins += 1;
+    } else if(computerMove == 'scissors') {
+        return losses += 1;
+    } else return ties += 1;
+}
+function playScissors(computerMove){
+    if(computerMove == 'paper') {
+        return wins += 1;
+    } else if(computerMove == 'rock') {
+        return losses += 1;
+    } else return ties += 1;
 }
 playerChoice.forEach((e) => e.addEventListener("click", play));
 
