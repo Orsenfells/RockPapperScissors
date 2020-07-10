65b59e2ea60e3335361a27bc8093d8c2c78fc19f
Leo <leonelsoto17@gmail.com>
2020-07-09 21:59:02 -0500

    added images to accompany the matches

--- v2/script.js
+++ v2/script.js
@@ -6,6 +6,12 @@ let computerMove  = document.querySelector('.computerMove')
 let playerWins    = document.querySelector('.wins');
 let computerWins  = document.querySelector('.losses');
 let totalTies     = document.querySelector('.ties');
+let rockImg     = new Image();
+let paperImg    = new Image();
+let scissorsImg = new Image();
+rockImg.src = 'images/rock.png';
+paperImg.src = 'images/paper.png';
+scissorsImg.src = 'images/scissors.png';
 let wins = 0;
 let losses = 0;
 let ties = 0;
@@ -15,22 +21,52 @@ function computerPlay() {
 function play() {
     let computer = computerPlay();
     let player = this.innerText.toLowerCase();
+    let playerImg;
     playerMove.innerText = player;
     computerMove.innerText = computer;
 
     switch(player) {
         case 'rock':
             playRock(computer);
+            appendImage(player, computer);
             break;
         case 'paper':
             playPaper(computer);
+            appendImage(player, computer);
             break;
         case 'scissors':
             playScissors(computer);
+            appendImage(player, computer);
             break;
     }
     score(); 
     return
+}
+function appendImage(player, computer) {
+    computerMove.textContent = "";
+    playerMove.textContent = "";
+    switch(computer) {
+        case 'rock':
+            computerMove.append(rockImg);
+            break;
+        case 'paper':
+            computerMove.append(paperImg);
+            break
+        case 'scissors':
+            computerMove.append(scissorsImg);
+            break
+    }
+    switch(player) {
+        case 'rock':
+            playerMove.append(rockImg);
+            break;
+        case 'paper':
+            playerMove.append(paperImg);
+            break;
+        case 'scissors':
+            playerMove.append(scissorsImg);
+            break;
+    }
 }
 function score() {
     playerWins.innerText = `Player Wins: ${wins}`;
@@ -38,24 +74,25 @@ function score() {
     totalTies.innerText = `Ties ${ties}`;
     return
 }
-function playRock(computerMove) {
-    if(computerMove == 'scissors') {
+function playRock(computerPlay) {
+    
+    if(computerPlay == 'scissors') {
         return wins += 1;
-    } else if(computerMove == 'paper') {
+    } else if(computerPlay == 'paper') {
        return losses += 1;
     } else return ties += 1;   
 }
-function playPaper(computerMove) {
-    if(computerMove == 'rock') {
+function playPaper(computerPlay) {
+    if(computerPlay == 'rock') {
         return wins += 1;
-    } else if(computerMove == 'scissors') {
+    } else if(computerPlay == 'scissors') {
         return losses += 1;
     } else return ties += 1;
 }
-function playScissors(computerMove){
-    if(computerMove == 'paper') {
+function playScissors(computerPlay){
+    if(computerPlay == 'paper') {
         return wins += 1;
-    } else if(computerMove == 'rock') {
+    } else if(computerPlay == 'rock') {
         return losses += 1;
     } else return ties += 1;
 }
